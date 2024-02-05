0e5476d70ac834b8d0c022c2c901f4a84f8742c8
DarklMoon <phuriwat757@gmail.com>
2024-02-05 10:04:48 +0700

    For push

--- src/app/(site)/signup/page.tsx
+++ src/app/(site)/signup/page.tsx
@@ -8,6 +8,7 @@ const Signup = (props: Props) => {
     <div className="flex min-h-screen justify-center flex-col items-center  p-24">
     <h1 className='bg-slate-600 p-2 text-white rounded-lg mb-3'>XD This is a Test Cicero XD SignUp</h1>
     <Button>Test for GIGA SHADCN</Button>
+    <div className='text-4xl  font-bold mt-5'>Test Merge Branch!</div>
     
   </div>
   )
