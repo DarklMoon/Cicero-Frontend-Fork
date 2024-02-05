a5e596a9994dd837c1b5896c2fc548f3714e691c
DarklMoon <phuriwat757@gmail.com>
2024-02-05 10:08:05 +0700

    Change-For-Merge-Test

--- src/app/(site)/signup/page.tsx
+++ src/app/(site)/signup/page.tsx
@@ -7,8 +7,7 @@ const Signup = (props: Props) => {
   return (
     <div className="flex min-h-screen justify-center flex-col items-center  p-24">
     <h1 className='bg-slate-600 p-2 text-white rounded-lg mb-3'>XD This is a Test Cicero XD SignUp</h1>
-    <Button>Test for GIGA SHADCN</Button>
-    
+    <div className='text-4xl font-bold mt-5'>I want to update this branch with changes from Main branch.</div>
   </div>
   )
 }
